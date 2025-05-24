ab8c246dc0b54f5814014677d29a90f3ae7f57a9
cypheredvortex <alimalik21realms@gmail.com>
2025-05-24 18:11:37 +0100

    Update views.py

--- commande/views.py
+++ commande/views.py
@@ -3,8 +3,8 @@ from django.http import JsonResponse
 from commande.models import Commande
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.cache import never_cache
-from user.models import UserProfile
 from stockmanagement.middleware import role_required  
+
 # Helper to disable cache
 def disable_cache(response):
     response['Cache-Control'] = 'no-cache, no-store, must-revalidate'
@@ -16,9 +16,6 @@ def disable_cache(response):
 @login_required(login_url='/loginpage/')
 @role_required(['employe'])  # Use the decorator correctly
 def get_commands(request):
-    if not request.user.is_authenticated or not user.is_employe:
-        return redirect('/loginpage/')
-    
     commands = Commande.objects.all()
     response = render(request, 'listcommands.html', {'commands': commands})
     return disable_cache(response)
